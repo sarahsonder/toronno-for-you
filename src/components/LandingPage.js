8f1ca8a71a495beb2dc9a50c6b926252ff28eb09
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-05 10:53:24 -0500

    FINAL COMMIT

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Buttons} from "./Buttons";
-//const link1 = "CoronationPark.jpg"
 
 const LandingPage = () => {
     return (
@@ -8,7 +7,7 @@ const LandingPage = () => {
             <h1>ToronnoForYou❣️</h1>
             <h2> Your one-stop-shop for all things Toronto Transit</h2>
             <Buttons/>
-            <img src="CoronationPark.jpg" alt="Picture of Toronto" />
+            <img src={"https://www.toronto.ca/wp-content/uploads/2017/07/8e31-explore-visitor-banner.jpg"} alt="Picture of Toronto" />
         </div>
     )
 }
