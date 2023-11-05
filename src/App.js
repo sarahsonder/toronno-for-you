af27385415165d135e42761410b0871525828f4b
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-05 03:00:52 -0500

    Added LandingPage and NavBar

--- src/App.js
+++ src/App.js
@@ -3,29 +3,14 @@ import {Buttons} from './components/Buttons'
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Trip_Planning from "./components/Trip_Planning";
+import LandingPage from "./components/LandingPage";
 
-const horCenter = {display: 'flex', alignItems: 'center', justifyContent: 'center'}
 const App = () => {
   return (
     <div className="App">
-        <div style={horCenter}>
-            <h1>ToronnoForYou❣️</h1>
-        </div>
-
-        <div style={horCenter}>
-            <h2> Your one-stop-shop for all things Toronto Transit</h2>
-        </div>
-
-        <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-        }}>
-            <Buttons/>
-        </div>
-
         <Routes>
-            <Route path="/trip-planning" element={<Trip_Planning/>}></Route>
+            <Route exact path="/" element={<LandingPage/>}></Route>
+            <Route exact path="/trip-planning" element={<Trip_Planning/>}></Route>
         </Routes>
     </div>
   );
