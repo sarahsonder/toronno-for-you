ec2d1dd8c16e77ef1c420b38c004b7493d2c5251
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2023-11-05 10:33:11 -0500

    Trying to add image

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {Buttons} from "./Buttons";
+//const link1 = "CoronationPark.jpg"
 
 const LandingPage = () => {
     return (
         <div className="landingPage">
-            <body>
-                <h1>ToronnoForYou❣️</h1>
-                <h2> Your one-stop-shop for all things Toronto Transit</h2>
-                <Buttons/>
-            </body>
+            <h1>ToronnoForYou❣️</h1>
+            <h2> Your one-stop-shop for all things Toronto Transit</h2>
+            <Buttons/>
+            <img src={"CoronationPark.jpg"} alt="Picture of Toronto" />
         </div>
     )
 }
