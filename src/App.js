8fef2f218c663c3f25fb4f0b96adc4118a24c39e
emmshen <emmy12821@gmail.com>
2023-11-05 00:00:45 -0400

    worked on buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import './App.css';
 
+function ButtonNewcomer() {
+  return (
+      <button style={{ width: "200px", height: "100px", fontSize: "25pt"}}>Crash Course</button>
+  )
+}
+
+function ButtonMap() {
+    return (
+        <button style={{ width: "200px", height: "100px", fontSize: "25pt"}}>Trip Planning</button>
+    )
+}
+
 function App() {
   return (
-    <div>ToronnoForYou❣️</div>
+    <div>
+        <div
+            style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <h1>ToronnoForYou❣️</h1>
+        </div>
+
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}><h2> Your one-stop-shop for all things Toronto Transit</h2></div>
+
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <ButtonNewcomer />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <ButtonMap />
+        </div>
+    </div>
   );
 }
 
