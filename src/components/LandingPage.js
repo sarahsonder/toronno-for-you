6492bc4ec813bec5354fe353303187cb156c12c4
Vanessa L. <weiweile2100@gmail.com>
2023-11-05 10:15:14 -0500

    body

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,9 +4,11 @@ import {Buttons} from "./Buttons";
 const LandingPage = () => {
     return (
         <div className="landingPage">
-            <h1>ToronnoForYou❣️</h1>
-            <h2> Your one-stop-shop for all things Toronto Transit</h2>
-            <Buttons/>
+            <body>
+                <h1>ToronnoForYou❣️</h1>
+                <h2> Your one-stop-shop for all things Toronto Transit</h2>
+                <Buttons/>
+            </body>
         </div>
     )
 }
